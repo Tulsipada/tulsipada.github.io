1b42a62b69a9ff5d6106c571f543038829d446e5
Tulsipada Das <tulsipada55@gmail.com>
2025-08-24 17:15:49 +0530

    Modified about

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,8 +1,8 @@
 export const aboutData = {
   title: "Who I am",
   description1:
-    "My name's Tulsipada. I'm a nodejs developer based in kolkata, IN.",
+    "I am Tulsipada Das, just a passionate Node.js developer who is experienced in the building of scalable backend systems as well as real-time applications plus secure payment integrations. Since I have a strong foundation in JavaScript, MySQL, as well as RESTful API design, I create efficient with reliable solutions that improve user experience.",
   description2:
-    "During the day I work as a NodeJs developer at a local agency and in the evening I  work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
+    "Because I am curious in my nature and I am driven by problem-solving, so I enjoy the projects that challenge me to innovate whether it's the developing of a high-performance web app, integrating of third-party services, or optimizing of databases for speed and scalability.",
   image: 1,
 };
